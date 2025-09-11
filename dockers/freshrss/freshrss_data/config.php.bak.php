931cb7e8086dc41c17cf6e1a042384d8039cc7aa
root <root@dockers.moore.lan>
2025-09-10 22:25:32 -0400

    Apply new .gitignore and clean repo

---
+++ dockers/freshrss/freshrss_data/config.php.bak.php
@@ -0,0 +1,19 @@
+<?php
+ return array (
+  'salt' => '6d0a038b637df9566d11a4d6becb0741766438c5',
+  'base_url' => 'http://192.168.50.251:8080',
+  'default_user' => '_',
+  'db' => 
+  array (
+    'type' => 'sqlite',
+    'host' => '',
+    'user' => '',
+    'password' => '',
+    'base' => '',
+    'prefix' => '',
+    'pdo_options' => 
+    array (
+    ),
+  ),
+  'pubsubhubbub_enabled' => false,
+);
